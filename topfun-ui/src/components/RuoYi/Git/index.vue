361b9a9fc8743a867cb9cb714b8a3080baafddd5
天明 <1036498144@qq.com>
2024-12-03 13:07:11 +0800

    docs: 添加项目功能设计和技术架构文档
    - 新增项目功能设计文档，包括博客记录网站和内置AI对话功能
    - 添加技术架构文档，涵盖前后端技术栈、数据库和AI技术
    - 创建项目开发步骤指南，从需求分析到部署上线
    - 新增多个批处理脚本文件，用于构建、打包和运行项目

---
+++ topfun-ui/src/components/RuoYi/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topfunGit',
+  data() {
+    return {
+      url: 'https://gitee.com/y_project/topfun-Vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
